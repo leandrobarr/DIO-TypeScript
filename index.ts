56aaa22ceb3542da58a5fb82969343e62105b145
leandrobarr <leandrobarr@yahoo.com.br>
2021-08-17 12:58:42 -0300

    atualizar

--- index.ts
+++ index.ts
@@ -1,1 +1,29 @@
-console.log('TypeScript');
+// types
+// interfaces
+
+interface IAnimal {
+    nome: string;
+    tipo: 'terrestre' | 'Aquático'
+    executarRugido (alturaEmDecibeis: number): void
+}
+
+interface IFelino extends IAnimal {
+     visaoNoturna: boolean
+}
+
+const animal: IAnimal = {
+    nome: 'Elefante',
+    tipo: 'terrestre',
+    executarRugido: (alturaEmDecibeis) => (`${alturaEmDecibeis}DB`)
+    
+}
+
+animal.executarRugido ('s')
+
+const felino: IFelino = {
+    nome: 'Leão',
+    tipo: 'terrestre',
+    visaoNoturna: true,
+    executarRugido: (alturaEmDecibeis) => (`${alturaEmDecibeis}DB`)
+
+}
